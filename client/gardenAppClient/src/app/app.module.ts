ad17d7b70b509c4de041e9b77d24816f210d44d7
Aurelia Carlin <aricarlin@gmail.com>
2018-10-24 17:13:06 -0400

    api called and displayed

--- client/gardenAppClient/src/app/app.module.ts
+++ client/gardenAppClient/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { PageProductService } from './page-product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    PageProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
