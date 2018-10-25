dfa49fa6e7fbf51b50a67e138aa085ef894169e0
Daniel Magley <danielmagley@Daniels-MacBook-Pro.local>
2018-10-25 11:30:31 -0400

    sign

--- client/gardenAppClient/src/app/auth/auth.component.ts
+++ client/gardenAppClient/src/app/auth/auth.component.ts
@@ -8,6 +8,11 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class AuthComponent implements OnInit {
   authForm: FormGroup;
+  submitted = false;
+  success = false;
+  values = "";
+  emailVal: string;
+  passwordVal: string;
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -16,5 +21,13 @@ export class AuthComponent implements OnInit {
       email: ["", Validators.required],
       password: ["", Validators.required]
     });
+  }
+
+  onSubmit() {
+    if (this.emailVal === "wer@odikepki.pr" && this.passwordVal === "test") {
+      this.success = true;
+    } else {
+      console.log("fail");
+    }
   }
 }
