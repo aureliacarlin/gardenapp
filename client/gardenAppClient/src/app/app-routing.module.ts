6ef59a7cf602b14cff44adf572e14d1e1ab15ab7
AaronStarrett <aaronjstarrett@gmail.com>
2018-10-25 01:02:06 -0400

    second commit with formspree and about page

--- client/gardenAppClient/src/app/app-routing.module.ts
+++ client/gardenAppClient/src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+// import { HomeComponent } from '../app/app.component';
 
- const routes: Routes = [
-// { path: "",
-// component: }
+
+
+const routes: Routes = [
+  // {
+  //   path: "",
+  //   component: HomeComponent
+  // },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  // {
+  //   path: "products",
+  //   component: ProductComponent
+  // },
+  // {
+  //   path: "signin",
+  //   component: SigninComponent
+  // }
 ];
 
 @NgModule({
